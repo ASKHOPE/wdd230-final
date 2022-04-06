306c9e986c6fcc3d467404dca4257cc5208563d5
ASKHOPE <96995931+ASKHOPE@users.noreply.github.com>
2022-04-07 01:54:31 +0530

    finish slide auto test one

--- scripts/slide-image.js
+++ scripts/slide-image.js
@@ -1,22 +1,22 @@
-var slide_index = 1;
-displaySlides(slide_index);
-function nextSlide(n) {
-  displaySlides((slide_index += n));
-}
-function currentSlide(n) {
-  displaySlides((slide_index = n));
-}
-function displaySlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("showSlide");
-  if (n > slides.length) {
-    slide_index = 1;
-  }
-  if (n < 1) {
-    slide_index = slides.length;
-  }
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("myImgSlides");
+  let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slide_index - 1].style.display = "block";
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
